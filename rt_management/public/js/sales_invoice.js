8f13851330aef73d7944461dfedbcb542993a184
Ramdani <xhijack@gmail.com>
2025-08-04 20:59:23 +0700

    fix sales invoice

--- rt_management/public/js/sales_invoice.js
+++ rt_management/public/js/sales_invoice.js
@@ -12,7 +12,7 @@ frappe.ui.form.on("Sales Invoice", {
                 if (r.message) {
                     const allowed_units = r.message;
 
-                    // Set filter untuk field unit pada item
+                    // Set filter untuk field unit pada semua baris item
                     frm.fields_dict.items.grid.get_field("unit").get_query = function(doc, cdt, cdn) {
                         return {
                             filters: [
@@ -21,10 +21,11 @@ frappe.ui.form.on("Sales Invoice", {
                         };
                     };
 
-                    // Jika minimal 1 unit, isi otomatis baris pertama
+                    // Jika minimal 1 unit, set semua item dengan unit pertama
                     if (allowed_units.length > 0 && frm.doc.items && frm.doc.items.length > 0) {
-                        const first_item = frm.doc.items[0];
-                        first_item.unit = allowed_units[0];
+                        frm.doc.items.forEach(item => {
+                            item.unit = allowed_units[0];
+                        });
                         frm.refresh_field("items");
                     }
                 }
